aed070158ffe737d6169217c40498cac1beeb3b6
Khaled <khaled@robotical.io>
2017-09-04 10:38:41 +0100

    Added comments

--- stand_up.py
+++ stand_up.py
@@ -1,47 +1,51 @@
 import martypy
 
 from time import sleep
-m = martypy.Marty('socket://192.168.0.43')
+m = martypy.Marty('socket://192.168.0.28')
 m.enable_motors()
+
+#Disable safeties to allow motors to consume the extra current needed to push Marty up
+'''
+Be Aware that the motor protection is there for a reason, to prevent you from breaking the motors.
+It can be surprisingly easy to destroy a microservo motor by forcing it to move when it doesn’t want to.
+We won’t cover warranty replacements for motors if you disable protections and then break a motor.
+'''
 m.enable_safeties(False)
-m.fall_protection(False)
+m.fall_protection(False)    #This is to stop Marty from turning off the motors when it detects the change in orientation.
 
 
-#Hands up
-m.move_joint(6, -127, 200)
-m.move_joint(7, -127, 200)
+#Rasie arms up
+m.move_joint(6, 127, 200)
+m.move_joint(7, 127, 200)
 
 #Bend knee
 m.move_joint(2, -100, 200)
 m.move_joint(5, 100, 200)
 sleep(1)
 
-#Lean Forward
+#Lean forward
 m.move_joint(0, 100, 200)
 m.move_joint(3, 100, 200)
 sleep(1)
 
-#Twist
+#Twist knees
 m.move_joint(1, -100, 200)
 m.move_joint(4, 100, 200)
 sleep(1)
 
-x = raw_input()
 m.enable_motors()
 
-#Hands down
-m.move_joint(6, 127, 200)
-m.move_joint(7, 127, 200)
+#Put arms down to push the body up
+m.move_joint(6, -127, 200)
+m.move_joint(7, -127, 200)
 
-x = raw_input()
+sleep(2)    #Wait for Marty to stablise
 
-#Twist back
+#Twist knees back
 m.move_joint(1, 10, 200)
 m.move_joint(4, 10, 200)
-sleep(1)
 
-x = raw_input()
-
+sleep(2)    #Wait for Marty to stablise
 
 #Pull legs in
 m.move_joint(2, 0, 200)
@@ -50,4 +54,8 @@ sleep(1)
 
 #Lean back
 m.move_joint(0, 0, 200)
-m.move_joint(3, 0, 200)
+m.move_joint(3, 0, 200
+
+#Re-enable safeties
+m.enable_safeties(True)
+m.fall_protection(True)
